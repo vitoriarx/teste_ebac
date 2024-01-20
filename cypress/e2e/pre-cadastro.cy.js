31d8c59b410febf23020f49d5e3ecbf48910a19d
Vitoria Regina <vitoriasantos78690@gmail.com>
2024-01-20 19:55:17 -0300

    alterações para usar fixture

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -7,7 +7,7 @@ describe('Funcionalidade pré cadastro', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
 
-    it('Deve fazer o pré cadastro', () => {
+    it('Deve fazer o pré cadastro com sucesso', () => {
         const fakeEmail = faker.internet.email();
         cy.get('#reg_email').type(fakeEmail);
         cy.get('#reg_password').type('@TesteTeste.com')
@@ -20,4 +20,10 @@ describe('Funcionalidade pré cadastro', () => {
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
+
+    it('Deve completar o pré-cadastro com sucesso usando comandos customizados', () => {
+        const fakeEmail2 = faker.internet.email();
+        cy.preCadastro(fakeEmail2, 'senha!@forte', 'Fábio', 'Araújo')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    })
 });
